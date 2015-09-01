b4ec252b68e3bd846489527a22c7f0881ad6e5f2
nagasudhirpulla <nagasudhirpulla@gmail.com>
2015-09-01 13:55:51 +0530

    Update MainSheetUI5.js

--- examples/MainSheetUI5.js
+++ examples/MainSheetUI5.js
@@ -645,6 +645,9 @@ function markCellsWithRevs()
  {
    //Add a row
    var tr = document.createElement('tr');
+   var td0 = document.createElement('td');
+   td0.appendChild(document.createTextNode(i+1);
+   tr.appendChild(td0);
    for(var constcol = 0;constcol<constituentNames.length;constcol++)
    {
      //Add cells to the table
