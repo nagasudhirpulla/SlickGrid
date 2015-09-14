3481908e340ab33fa68b8e1388b87b5897d193bc
nagasudhirpulla <nagasudhirpulla@gmail.com>
2015-09-14 12:39:29 +0530

    URSRSD  solving incorporated ,algorithm changed

--- examples/distributingRamp.js
+++ examples/distributingRamp.js
@@ -1,10 +1,9 @@
-function handle(e)
-{
+function handle(e) {
 	Catagorise();
 	return false;
 }
 
-function Catagorise(){
+function Catagorise() {
 	//Revisions of all the cells in a particular row
 	//var rowRevs = [1, 1, 1, 3, 4, 5, 4, 3];
 	var rowRevs = document.getElementById('RevsIn').value.split(",");
@@ -18,7 +17,7 @@ function Catagorise(){
 	maxCellVals = convertToNums(maxCellVals);
 	var maxShareVals = document.getElementById('CellSharesIn').value.split(",");
 	maxShareVals = convertToNums(maxShareVals);
-	var modifiedRowRevVals = solveRamping(maxShareVals,rowRevs,rowRevVals,rowPrevRevVals,maxCellVals,maxRampOfRow);
+	var modifiedRowRevVals = solveRamping(maxShareVals, rowRevs, rowRevVals, rowPrevRevVals, maxCellVals, maxRampOfRow);
 
 	document.getElementById('console').innerHTML = '';
 	document.getElementById('console').appendChild(document.createTextNode('Revision_Numbers_Data=' + JSON.stringify(segregateColumns(rowRevs), null, '\t') + '------' + getRevNumsinDescOrder(segregateColumns(rowRevs))));
@@ -34,18 +33,19 @@ function convertToNums(arr) {
 	return temp;
 }
 
-function solveRamping(maxShareVals,rowRevs,rowRevVals,rowPrevRevVals,maxCellVals,maxRampOfRow) {	
+function solveRamping(maxShareVals, rowRevs, rowRevVals, rowPrevRevVals, maxCellVals, maxRampOfRow, onBar) {
 	//An object created to catagorise the row cells according to the revision number in descending order
 	var rowRevsCatagorised = segregateColumns(rowRevs);
 	//An array initialised which has to contain the rev number present in the row in descending order
 	var rowRevsDupExcluded = getRevNumsinDescOrder(rowRevsCatagorised);
 	//document.getElementById('console').innerHTML = '';
 	//document.getElementById('console').appendChild(document.createTextNode('Revision_Numbers_Data=' + JSON.stringify(rowRevsCatagorised, null, '\t') + '------' + rowRevsDupExcluded));
-	
+
 	//Now we have the  revision numbers ordered and categorised	
 	var modifiedRowRevVals = rowRevVals;
 	var cellRamp = 0;
 	var rampOfRow = 0;
+	var sumgenPrev = 0; //URS Version
 	//Now modify the present rev values to stisfy *hard constraints* and then calculate the ramp of the row
 	for (var i = 0; i < rowRevVals.length; i++) {
 		if (rowRevVals[i] < 0) {
@@ -56,24 +56,30 @@ function solveRamping(maxShareVals,rowRevs,rowRevVals,rowPrevRevVals,maxCellVals
 		}
 		cellRamp = rowRevVals[i] - rowPrevRevVals[i];
 		rampOfRow += cellRamp;
+		sumgenPrev += rowPrevRevVals[i]; //URS Version
 	}
 	//modify the present rev values to satisfy *hard constraints* and ramp of row calculation over
 	var rampDiffOfRow = 0;
-	if(rampOfRow>=0)
-	{
+	if (rampOfRow >= 0) {
+		//This is ramping Up Case...
+		//URS Version
+		//Modify maxramp to satisfy that sumgen  doesnot be greater than onBarDC
+		if (sumgenPrev + maxRampOfRow > onBar) {
+			maxRampOfRow = onBar - sumgenPrev;
+		}
+		//URS Version
 		rampDiffOfRow = rampOfRow - maxRampOfRow; //rampDiffOfRow is possitive if rampedUp more than maxRampOfRow
-	}
-	else{//rampOfRow neagtive
+	} else { //rampOfRow neagtive
 		rampDiffOfRow = rampOfRow + maxRampOfRow; //rampDiffOfRow is negative if rampedUp more than maxRampOfRow
 	}
-	
-	if((rampDiffOfRow<=0 && rampOfRow>=0)||(rampDiffOfRow>=0 && rampOfRow<=0)) //then no problem!!!
+
+	if ((rampDiffOfRow <= 0 && rampOfRow >= 0) || (rampDiffOfRow >= 0 && rampOfRow <= 0)) //then no problem!!!
 	{
 
 	} else {
 		//sacrificing from bigger revision till smallest revision
 		var rampUpByRevCells;
-		for (var i = 0; i < rowRevsDupExcluded.length && Math.abs(rampDiffOfRow)>0.1; i++) {
+		for (var i = 0; i < rowRevsDupExcluded.length && Math.abs(rampDiffOfRow) > 0.1; i++) {
 			//Access the *columns of a specific revision* number			
 			var colNums = rowRevsCatagorised[rowRevsDupExcluded[i]].cols;
 			//Get the amount of ramp up they have been through
@@ -82,11 +88,11 @@ function solveRamping(maxShareVals,rowRevs,rowRevVals,rowPrevRevVals,maxCellVals
 				//TODO do negative ramped cell segregation and updating the rampUp benifit of rampedUpCells here.
 				rampUpByRevCells += rowRevVals[colNums[j]] - rowPrevRevVals[colNums[j]];
 			} //amount of rampUp the cells of a particular revision have been through is found out
-			
+
 			//Now make the rev to sacrifice rampUp and update the rampOfRow value.
 			//Maximum rampUp sacrifice will be equal to rampUpByRevCells.
 			var rampUpToSacrifice = 0;
-			if (((rampDiffOfRow <= rampUpByRevCells) && rampDiffOfRow>=0)||((rampDiffOfRow >= rampUpByRevCells) && rampDiffOfRow<=0)) //Then the row can sacrifice the complete excess ramp up
+			if (((rampDiffOfRow <= rampUpByRevCells) && rampDiffOfRow >= 0) || ((rampDiffOfRow >= rampUpByRevCells) && rampDiffOfRow <= 0)) //Then the row can sacrifice the complete excess ramp up
 			{
 				rampUpToSacrifice = rampDiffOfRow;
 				rampDiffOfRow = 0;
@@ -108,16 +114,16 @@ function solveRamping(maxShareVals,rowRevs,rowRevVals,rowPrevRevVals,maxCellVals
 				var colsToRampUp = [];
 				for (var k = 0; k < colNums.length; k++) {
 					//if the column cell wants to ramp up
-					if ((rowRevVals[colNums[k]] - rowPrevRevVals[colNums[k]] > 0) && rampDiffOfRow>=0)
+					if ((rowRevVals[colNums[k]] - rowPrevRevVals[colNums[k]] > 0) && rampDiffOfRow + rampUpToSacrifice > 0)
 						colsToRampUp.push(colNums[k]);
-					else if((rowRevVals[colNums[k]] - rowPrevRevVals[colNums[k]] < 0) && rampDiffOfRow<=0)//redundant...
+					else if ((rowRevVals[colNums[k]] - rowPrevRevVals[colNums[k]] < 0) && rampDiffOfRow + rampUpToSacrifice < 0) //redundant...
 						colsToRampUp.push(colNums[k]);
 				}
 				//Now we know the columns to ramp up in a particular revision.
 				//Now try to distribute the ramp up according to rampUpShare of each cell and update iteration ramp up and toRampUpPrev
 				for (var k = 0; k < colsToRampUp.length; k++) {
 					var ramptocell = toRampUpPrev * shareincolstorampup(maxShareVals, colsToRampUp[k], colsToRampUp); //todo function
-					if (((ramptocell > rowRevVals[colsToRampUp[k]]-rowPrevRevVals[colsToRampUp[k]])&&ramptocell>0) || ((ramptocell < rowRevVals[colsToRampUp[k]]-rowPrevRevVals[colsToRampUp[k]])&&ramptocell<0)) {
+					if (((ramptocell > rowRevVals[colsToRampUp[k]] - rowPrevRevVals[colsToRampUp[k]]) && ramptocell > 0) || ((ramptocell < rowRevVals[colsToRampUp[k]] - rowPrevRevVals[colsToRampUp[k]]) && ramptocell < 0)) {
 						ramptocell = rowRevVals[colsToRampUp[k]] - rowPrevRevVals[colsToRampUp[k]];
 					}
 					modifiedRowRevVals[colsToRampUp[k]] = rowPrevRevVals[colsToRampUp[k]] + ramptocell;
@@ -158,9 +164,8 @@ function getRevNumsinDescOrder(rowRevsCatagorised) {
 function shareincolstorampup(maxShareVals, colsToRampUpIndex, colsToRampUp) {
 	//return 1 / colsToRampUp.length;
 	var shareSum = 0;
-	for(var i=0;i<colsToRampUp.length;i++)
-	{
-		shareSum += maxShareVals[colsToRampUp[i]];
+	for (var i = 0; i < colsToRampUp.length; i++) {
+		shareSum += maxShareVals[colsToRampUp[i] % maxShareVals.length];
 	}
-	return maxShareVals[colsToRampUpIndex]/shareSum;
+	return maxShareVals[colsToRampUpIndex % maxShareVals.length] / shareSum;
 }
