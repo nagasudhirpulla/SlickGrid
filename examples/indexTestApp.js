4bb6b88f6b8eea7f811ba5ced5702f3b19116199
nagasudhirpulla <nagasudhirpulla@gmail.com>
2015-09-21 23:24:16 +0530

    Update indexTestApp.js

--- examples/indexTestApp.js
+++ examples/indexTestApp.js
@@ -3,23 +3,32 @@ databaseOpen(function(){
 });
 
 
-function createnew1()
+function createnew()
 {
 	dataObj = {str:document.getElementById("textinput").value};
-	databaseRevsAdd(count+1,dataObj,function(){
-			count = count + 1;
-                alert("Revision "+count+"created with the saved revision data of Revision "+(count-1)+"...");
-            });
+	addNewRevision(function(revdata){
+			console.log("Revision "+count+" created...");
+			console.log(revdata);//logically revData = dataObj
+            },dataObj);
 }
 function loadrev()
 {
-	loadRevision(+document.getElementById("textinput").value, function(dataObj){
-		console.log(dataObj.revData);
-	});
+	loadRevision(+document.getElementById("textinput").value, function(record){
+		console.log(record.revData);
+	},+document.getElementById("textinput").value);
 }
 
-function createnew(){
-	loadRevision(count+1,function(){
+function updaterev(){
+	var modifyFunction = function(oldRec){
+		oldRec.revData = Date.now();
+		return oldRec;
+	};
+	var onCompleteFunction = function(updatedRec){
+		console.log(updatedRec);
+	};
+	updateRev(+document.getElementById("textinput").value,modifyFunction,onCompleteFunction);
+}
 
-	});
+function deleterev(){
+	deleteRev(+document.getElementById("textinput").value);
 }
